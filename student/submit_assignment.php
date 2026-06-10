05a5270b904a7cf165faec021509bf92c8984ad5
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-06-10 12:00:35 +0100

    Create submit_assignment.php

---
+++ student/submit_assignment.php
@@ -0,0 +1,35 @@
+<?php
+require_once "../config/database.php";
+require_once "../config/auth.php";
+
+requireLogin();
+
+$user_id = $_SESSION['user_id'];
+
+$assignment_id = $_POST['assignment_id'];
+$answer = $_POST['answer'];
+
+$file_path = null;
+
+if(!empty($_FILES['file']['name'])){
+    $file = time() . "_" . $_FILES['file']['name'];
+    $tmp = $_FILES['file']['tmp_name'];
+
+    $file_path = "../uploads/assignments/" . $file;
+    move_uploaded_file($tmp, $file_path);
+}
+
+$stmt = $pdo->prepare("
+INSERT INTO submissions
+(assignment_id,user_id,answer,file)
+VALUES (?,?,?,?)
+");
+
+$stmt->execute([
+    $assignment_id,
+    $user_id,
+    $answer,
+    $file_path
+]);
+
+echo "Submitted successfully";
