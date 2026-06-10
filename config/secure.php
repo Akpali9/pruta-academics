6db2c77b1af11e8a1c3f6903038613bcafb03680
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-06-10 12:53:41 +0100

    Update secure.php

--- config/secure.php
+++ config/secure.php
@@ -1,12 +1,17 @@
 <?php
 
-function securePage()
-{
-    if(session_status() === PHP_SESSION_NONE){
-        session_start();
-    }
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-    if(!isset($_SESSION['user_id'])) {
+function requireAuth()
+{
+    if (!isset($_SESSION['user_id'])) {
+
+        // prevent cached protected pages
+        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+        header("Pragma: no-cache");
+
         header("Location: /login.php");
         exit;
     }
