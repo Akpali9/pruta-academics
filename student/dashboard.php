92a60ae640e4aa370e11037102ae7fcc79819087
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-06-10 11:50:13 +0100

    Create dashboard.php

---
+++ student/dashboard.php
@@ -0,0 +1,12 @@
+<?php
+require_once "../config/database.php";
+require_once "../config/auth.php";
+
+requireLogin();
+?>
+
+<h2>Welcome <?= $_SESSION['fullname'] ?></h2>
+
+<a href="courses.php">View Courses</a> |
+<a href="access.php">My Access</a> |
+<a href="../logout.php">Logout</a>
