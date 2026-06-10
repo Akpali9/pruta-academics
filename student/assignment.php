1739253b8abff8606acbbcdb1cba57a4c46c7a3c
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-06-10 12:00:04 +0100

    Create assignment.php

---
+++ student/assignment.php
@@ -0,0 +1,31 @@
+<?php
+require_once "../config/database.php";
+require_once "../config/auth.php";
+
+requireLogin();
+
+$module_id = $_GET['module_id'];
+
+$stmt = $pdo->prepare("
+SELECT * FROM assignments WHERE module_id=?
+");
+$stmt->execute([$module_id]);
+
+$assignment = $stmt->fetch();
+?>
+
+<h2>Assignment</h2>
+
+<p><?= $assignment['question'] ?></p>
+
+<form method="POST" action="submit_assignment.php" enctype="multipart/form-data">
+
+<input type="hidden" name="assignment_id" value="<?= $assignment['id'] ?>">
+
+<textarea name="answer" placeholder="Write answer"></textarea>
+
+<input type="file" name="file">
+
+<button type="submit">Submit</button>
+
+</form>
