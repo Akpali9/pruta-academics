29984ab363211d7a3c049c5f1a62e11fe4e93211
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-06-10 11:44:30 +0100

    Insert initial user data into users table

---
+++ schema.sql
@@ -0,0 +1,18 @@
+INSERT INTO users
+(
+ fullname,
+ email,
+ phone,
+ password,
+ role,
+ status
+)
+VALUES
+(
+ 'System Admin',
+ 'admin@pruta.com',
+ '08000000000',
+ '$2y$10$JfA7oS0dN4k4dQh1x7zFQeM3N2JQjVx4x9N0Fv3k0x4y2V0Y8lJ7m',
+ 'nigeria',
+ 'active'
+);
