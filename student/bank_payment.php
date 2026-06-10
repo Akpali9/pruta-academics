a05daf456eed1c1ef078ce841679e73ec3cc79e0
Akpali Francis Ezennaya <francisakpali9@gmail.com>
2026-06-10 12:28:54 +0100

    Create bank_payment.php

---
+++ student/bank_payment.php
@@ -0,0 +1,30 @@
+<?php
+require_once "../config/database.php";
+require_once "../config/auth.php";
+
+requireLogin();
+
+$course_id = $_GET['course_id'];
+?>
+
+<h2>Bank Payment</h2>
+
+<h3>Send payment to:</h3>
+
+<p>
+Bank: GTBank <br>
+Account: 0123456789 <br>
+Name: Your Company Name
+</p>
+
+<form method="POST" action="submit_bank_payment.php" enctype="multipart/form-data">
+
+<input type="hidden" name="course_id" value="<?= $course_id ?>">
+
+<input type="text" name="bank_reference" placeholder="Bank Transfer Reference" required>
+
+<input type="file" name="receipt" required>
+
+<button type="submit">Submit Payment</button>
+
+</form>
